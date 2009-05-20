8aec21d6815420c4dfe84d8327f1ba2e93910954
Chris Scharf <scharfie@gmail.com>
2009-05-20 14:26:26 -0400

    Readme, etc

---
+++ lib/active_record/has_default.rb
@@ -0,0 +1,33 @@
+module ActiveRecord
+  module HasDefault
+    module ClassMethods
+      attr_accessor :has_default_values
+    
+      def has_default_values
+        @has_default_values ||= {}
+      end
+    
+      def has_default(attribute, &block)
+        has_default_values[attribute] = block
+      
+        define_method(attribute) do
+          block.call(self)
+        end
+      end
+    end
+  
+    module InstanceMethods
+      def ensure_has_default_values
+        self.class.has_default_values.each do |attribute, block|
+          block.call(self)
+        end
+      end
+    end
+  
+    def self.included(receiver)
+      receiver.extend         ClassMethods
+      receiver.send :include, InstanceMethods
+      receiver.send :before_validation, :ensure_has_default_values
+    end
+  end
+end
